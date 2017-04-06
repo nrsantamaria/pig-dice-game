59c73a4e5e4cbfa98bae057f35f1309d141602c2
Asia Kane and Nicole Santamaria <asia@>
2017-04-06 10:06:09 -0700

    create game object and refactor logic to utilize currentPlayer property of game object

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 //business logic
-function Player(totalScore) {
+function Game (currentPlayer) {
+  this.currentPlayer = currentPlayer;
+};
+
+function Player(tempScore, totalScore) {
+  this.tempScore = tempScore;
   this.totalScore = totalScore;
 };
 
@@ -9,21 +14,20 @@ function roll(){
 };
 
 // Evaluates if number is equal to one, return value = 0.  Else, add number to value and return value.
-function updateValue (number) {
-  var value = 0;
+Player.prototype.updateValue = function(number) {
   if (number === 1){
-    value = 0;
+    this.tempScore = 0;
+    $(".btn-danger").toggle();
   } else {
-    value += number;
+    this.tempScore += number;
   }
-  return value;
 };
 
 //user interface logic
 $(document).ready(function(){
-  var playerOne = new Player (0);
-  var playerTwo = new Player (0);
-  var tempScore = 0;
+  var playerOne = new Player (0,0);
+  var playerTwo = new Player (0,0);
+  var newGame = new Game (playerOne);
 
   $("#player-one-score").text("Total score is = " + playerOne.totalScore);
   $("#player-two-score").text("Total score is = " + playerTwo.totalScore);
@@ -35,43 +39,43 @@ $(document).ready(function(){
     $("#roll-value").text(rollValue); //Display roll value
 
     //Update tempScore for either player
-    var temporary = updateValue(rollValue);
-    if (temporary === 0){
-      tempScore = 0;
-      $(".btn-danger").toggle();
-    } else {
-      tempScore += temporary;
-    };
+    newGame.currentPlayer.updateValue(rollValue);
 
     //Display tempScore
-    $("#temp-score").text("Temporary score is = " + tempScore);
+    $("#temp-score").text("Temporary score is = " + newGame.currentPlayer.tempScore);
 
     //Hold button to update totalScore and clear tempScore for Player One
     $("#hold1").click(function(){
       $("#hold1").hide();
       $("#hold2").show();
-      playerOne.totalScore += tempScore;
-      tempScore = 0;
+      playerOne.totalScore += playerOne.tempScore;
+      playerOne.tempScore = 0;
       $("#player-one-score").text("Total score is = " + playerOne.totalScore);
+      $("#temp-score").text("Temporary score is = " + playerOne.tempScore);
+
       //Player one wins!
       if (playerOne.totalScore >= 100){
         $("#player-one-score").text("You Win!")
-        $("#player-two-score").text("You Lose :(")
+        $("#player-two-score").text("You Lose")
       };
+      newGame.currentPlayer = playerTwo;
     });//Hold button close
 
     //Hold button to update totalScore and clear tempScore for Player Two
     $("#hold2").click(function(){
       $("#hold2").hide();
       $("#hold1").show();
-      playerTwo.totalScore += tempScore;
-      tempScore = 0;
+      playerTwo.totalScore += playerTwo.tempScore;
+      playerTwo.tempScore = 0;
       $("#player-two-score").text("Total score is = " + playerTwo.totalScore);
+      $("#temp-score").text("Temporary score is = " + playerTwo.tempScore);
+
       //Player two wins!
       if (playerTwo.totalScore >= 100){
-        $("#player-one-score").text("You Lose :(")
+        $("#player-one-score").text("You Lose")
         $("#player-two-score").text("You Win!")
       };
+      newGame.currentPlayer = playerOne;
     });//Hold button close
   });//form submit close
 });//document ready close
